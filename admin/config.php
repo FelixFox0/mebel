51f58641bc97b6372e2b655906705c7889eaec46
FelixFox0 <ssseee795@gmail.com>
2018-11-01 22:24:41 +0200

    first commit

--- admin/config.php
+++ admin/config.php
@@ -29,6 +29,6 @@ define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'opencart_base');
+define('DB_DATABASE', 'mebel');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
