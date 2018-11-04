87f090de77485bc369436bfc814c81080520c14a
FelixFox0 <ssseee795@gmail.com>
2018-11-04 23:39:44 +0200

    config

--- admin/config.php
+++ admin/config.php
@@ -26,9 +26,9 @@ define('DIR_CATALOG', $server_document_root.'/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
+define('DB_HOSTNAME', '94.130.104.114');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'HTURy2aSnpNs83$');
 define('DB_DATABASE', 'mebel');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
