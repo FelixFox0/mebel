828752c0413455a1c4af53e26d4da02d97ae91ea
FelixFox0 <ssseee795@gmail.com>
2018-12-15 11:48:53 +0200

    new config

--- config.php
+++ config.php
@@ -23,12 +23,15 @@ define('DIR_UPLOAD', $server_document_root.'/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-//define('DB_HOSTNAME', '94.130.104.114');
-define('DB_HOSTNAME', 'localhost');
-//define('DB_USERNAME', 'mebel');
-define('DB_USERNAME', 'root');
-//define('DB_PASSWORD', 'HTURy2aSnpNs!$');
-define('DB_PASSWORD', '');
+
+//define('DB_HOSTNAME', 'localhost');
+//define('DB_USERNAME', 'root');
+//define('DB_PASSWORD', '');
+
+define('DB_HOSTNAME', '94.130.104.114');
+define('DB_USERNAME', 'mebel');
+define('DB_PASSWORD', 'HTURy2aSnpNs!$');
+
 define('DB_DATABASE', 'mebel');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
