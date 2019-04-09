de92a0e271a438f599af39ee4b0de83c9184684a
FelixFox0 <ssseee795@gmail.com>
2019-04-09 20:19:37 +0300

    config local db

--- config.php
+++ config.php
@@ -24,21 +24,21 @@ define('DIR_UPLOAD', $server_document_root.'/system/storage/upload/');
 // DB
 define('DB_DRIVER', 'mysqli');
 
-//define('DB_HOSTNAME', 'localhost');
-//define('DB_USERNAME', 'root');
-//define('DB_PASSWORD', '');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
 
 //define('DB_HOSTNAME', '94.130.104.114');
 //define('DB_USERNAME', 'mebel');
 //define('DB_PASSWORD', 'HTURy2aSnpNs!$');
 
-//define('DB_DATABASE', 'mebel');
+define('DB_DATABASE', 'mebel');
 
 
-define('DB_HOSTNAME', 'dn333442.mysql.tools');
-define('DB_USERNAME', 'dn333442_mebel');
-define('DB_PASSWORD', '1hV8p;!eE6');
-define('DB_DATABASE', 'dn333442_mebel');
+//define('DB_HOSTNAME', 'dn333442.mysql.tools');
+//define('DB_USERNAME', 'dn333442_mebel');
+//define('DB_PASSWORD', '1hV8p;!eE6');
+//define('DB_DATABASE', 'dn333442_mebel');
 
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
